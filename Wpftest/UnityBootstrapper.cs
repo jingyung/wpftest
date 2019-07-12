a397ba7ad911282b349b089c7cb2d74a509b08d4
peter <pan710720@yahoo.com.tw>
2019-07-12 16:34:10 +0800

    feat add prism test

--- Wpftest/UnityBootstrapper.cs
+++ Wpftest/UnityBootstrapper.cs
@@ -1,7 +1,9 @@
 ﻿using Unity;
 using Prism.Unity;
- 
+
 using System.Windows;
+using Prism.Modularity;
+using Prism.Mvvm;
 
 namespace BootstrapperShell
 {
@@ -9,12 +11,20 @@ namespace BootstrapperShell
     {
         protected override DependencyObject CreateShell()
         {
-            return Container.Resolve<Wpftest.MainWindow>();
+            return Container.Resolve<Wpftest.PrismWindow>();
         }
 
         protected override void InitializeShell()
         {
             Application.Current.MainWindow.Show();
+        }
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            this.ModuleCatalog.AddModule<ModuleA.ModuleAModule>();
+            ViewModelLocationProvider.Register<ModuleA.Views.ViewC, ModuleA.ViewModels.ViewCViewMode9l>();
+
+
         }
     }
 }
